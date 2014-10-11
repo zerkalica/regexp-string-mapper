a9e205957800e869ad5767bbb9c8c6144a61eacc
Stefan Zerkalica <zerkalica@gmail.com>
2014-10-11 18:54:10 +0400

    first version

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/regexp-string-mapper');
---
+++ tests/regexp-string-mapper-test.js
@@ -0,0 +1,23 @@
+var expect = require('./test-helpers').expect;
+var RegExpStringMapper = require('../lib/regexp-string-mapper');
+
+describe('RegExpStringMapper', function () {
+  var mapper = RegExpStringMapper();
+
+  var testMessage = '%time: YYYY-MM-DD% %testId% %testObj% %testObj.a% %testObj.e% %%rrr%%';
+  var testTokens = {
+    time: new Date('2014-10-11T11:51:56.822Z'),
+    testId: 123,
+    testObj: {
+      a: 1,
+      b: [1, 2]
+    }
+  };
+  var testResult = '2014-10-11 123 {\"a\":1,\"b\":[1,2]} 1 %rrr%';
+
+  describe('#map', function () {
+    it('should map tokens', function () {
+      mapper.map(testMessage, testTokens).should.to.be.equal(testResult);
+    });
+  });
+});
